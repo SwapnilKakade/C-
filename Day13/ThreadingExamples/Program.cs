35b3c5f17b6f369359c259fa0239b0452fe4303c
Swapnil kakade <131379440+SwapnilKakade@users.noreply.github.com>
2023-12-25 23:18:36 +0530

    Add files via upload

---
+++ Day13/ThreadingExamples/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadingExamples1
+{
+    internal class Program
+    {
+        static void Main1(string[] args)
+        {
+            //Thread t = new Thread(new ThreadStart(Func1));
+            Thread t1 = new Thread(Func1);
+            Thread t2 = new Thread(Func2);
+            //t1.Priority = ThreadPriority.Highest;
+            //t1.IsBackground = true;
+            t1.Start();
+            t2.Start();
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Main " + i);
+                //if(i == 50)
+                //{
+                //    t1.Abort();
+                //}
+            }
+            //t1.Suspend();
+            //t1.Resume();
+
+
+            t1.Join();            // This code should run only after func1 is over
+            Console.WriteLine("This code should run only after fuc1 is over");
+
+            //Console.WriteLine( t1.ThreadState);
+
+           // if(t1.ThreadState == ThreadState.) ;
+        }
+        static void Func1()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("First "+i);
+                Thread.Sleep(1000);
+            }
+        }
+        static void Func2()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Second " + i);
+            }
+        }
+    }
+}
+
+namespace ThreadingExamples2
+{
+    internal class Program
+    {
+        static void Main1(string[] args)
+        {
+            Thread t1 = new Thread(new ParameterizedThreadStart(Func1));
+            Thread t2 = new Thread(Func2);
+         
+            t1.Start(" o1");
+            t2.Start(" o2");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Main " + i.ToString());
+                //if(i == 50)
+                //{
+                //    t1.Abort();
+                //}
+            }
+           
+        }
+        static void Func1(object o)
+        {
+             
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("First " + i.ToString() + o.ToString());
+                Thread.Sleep(1000);
+            }
+        }
+        static void Func2(object o)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Second " + i.ToString() + o.ToString());
+            }
+        }
+    }
+}
+
+namespace ThreadingExamples3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ThreadPool.QueueUserWorkItem(new WaitCallback(PoolFunc1), " aaa");
+            ThreadPool.QueueUserWorkItem(PoolFunc1, " aa");
+            //ThreadPool.QueueUserWorkItem(new WaitCallback(PoolFunc1));
+            ThreadPool.QueueUserWorkItem(PoolFunc2);
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Main " + i.ToString());
+            }
+            int workerthreads, iothreads;
+            ThreadPool.GetAvailableThreads(out workerthreads, out iothreads);
+
+            //ThreadPool.SetMinThreads;
+            //ThreadPool.SetMaxThreads;
+            //ThreadPool.GetMinThreads;
+
+            Console.WriteLine(workerthreads);
+            Console.WriteLine(iothreads);
+
+        }
+        static void PoolFunc1(object o)
+        {
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("First " + i.ToString() + o.ToString());
+              
+            }
+        }
+        static void PoolFunc2(object o)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Second " + i.ToString() + o.ToString());
+            }
+        }
+    }
+}
---
+++ Day13/fgh/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqExample
+{
+    internal class Program
+    {
+        static List<Employee> lstEmp = new List<Employee>();
+        static List<Department> lstDept = new List<Department>();
+
+        public static void AddRecs()
+        {
+            lstDept.Add(new Department { DeptNo = 10, DeptName = "Sales" });
+            lstDept.Add(new Department { DeptNo = 20, DeptName = "IT" });
+            lstDept.Add(new Department { DeptNo = 30, DeptName = "HR" });
+
+
+            lstEmp.Add(new Employee { EmpNo = 1, Name = "A", Basic = 10000, DeptNo = 10, Gender = "M" });
+            lstEmp.Add(new Employee { EmpNo = 2, Name = "B", Basic = 3000, DeptNo = 30, Gender = "F" });
+            lstEmp.Add(new Employee { EmpNo = 3, Name = "C", Basic = 50000, DeptNo = 20, Gender = "M" });
+            lstEmp.Add(new Employee { EmpNo = 4, Name = "D", Basic = 20000, DeptNo = 10, Gender = "F" });
+            lstEmp.Add(new Employee { EmpNo = 5, Name = "E", Basic = 30000, DeptNo = 30, Gender = "F" });
+            lstEmp.Add(new Employee { EmpNo = 6, Name = "C", Basic = 40000, DeptNo = 20, Gender = "M" }); ;
+        }
+
+        static Employee GetEmployees(Employee o)
+        {
+            return o;
+        }
+
+        static string GetEmployees2(Employee o)
+        {
+            return o.Name;
+        }
+        static void Main1(string[] args)
+        {
+            AddRecs();
+            //Employee emp = lstEmp.Single(e => e.EmpNo == 1);                      // works
+            //Employee emp = lstEmp.Single(e =>  e.EmpNo == 15);                    //Not found exception
+            //Employee emp = lstEmp.Single(e => e.Basic > 10000);                   //Multiple exception
+            //Employee emp = lstEmp.SingleOrDefault(e => e.Basic > 10000);          //Multiple exception
+            //Employee emp = lstEmp.SingleOrDefault(e => e.EmpNo == 1);                      // works
+            Employee emp = lstEmp.SingleOrDefault(e =>  e.EmpNo == 15);                    //Not found
+            if(emp != null)
+            {
+                Console.WriteLine(emp);
+            }
+            else
+            {
+                Console.WriteLine("Not found");
+            }
+
+            Console.ReadLine();
+        }
+
+        static void Main2()
+        {
+            AddRecs();
+
+            var emps = from emp in lstEmp select emp;
+
+            emps = emps.ToList();  //.ToArray .ToDictionary
+
+            Console.WriteLine();
+
+            lstEmp.RemoveAt(0);
+            foreach(var  emp in emps)
+            {
+                Console.WriteLine(emp);
+            }
+            
+            Console.WriteLine();
+
+            lstEmp.Add(new Employee { EmpNo = 15,Name = "New" , Basic = 2550 , DeptNo = 20,Gender = "M" });
+            foreach (var emp in emps)
+            {
+                Console.WriteLine(emp);
+            }
+
+
+            Console.ReadLine();
+        }
+
+   
+    }
+    public class Department
+    {
+        public int DeptNo { get; set; }
+        public string DeptName { get; set; }
+
+        public override string ToString()
+        {
+            return $"DeptNo: {DeptNo}, DeptName: {DeptName}";
+        }
+    }
+    public class Employee
+    {
+        public int EmpNo { get; set; }
+        public string Name { get; set; }
+        public decimal Basic { get; set; }
+        public int DeptNo { get; set; }
+        public string Gender { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name},{EmpNo},{Basic},{DeptNo},{Gender}";
+        }
+
+
+    }
+}
